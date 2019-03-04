7280a8fbf3267a8ea34ccdb8678ef41d94b7b7ee
brellin <cekakid@gmail.com>
2019-03-04 11:37:42 -0600

    changed ALL 'let' to 'const' and 'getElementsByTagName' to 'querySelectorAll'

--- js/index.js
+++ js/index.js
@@ -38,11 +38,11 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
+const logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 // Link Text
-let links = document.getElementsByTagName("a");
+const links = document.querySelectorAll("a");
 links[0].innerText = siteContent["nav"]["nav-item-1"];
 links[1].innerText = siteContent["nav"]["nav-item-2"];
 links[2].innerText = siteContent["nav"]["nav-item-3"];
@@ -52,64 +52,64 @@ links[5].innerText = siteContent["nav"]["nav-item-6"];
 
 // ====================="CTA"=======================
 // h1
-let ctaH1 = document.getElementsByTagName("h1")[0];
+const ctaH1 = document.querySelectorAll("h1")[0];
 ctaH1.innerText = siteContent["cta"]["h1"];
 // Button
-let headerButton = document.getElementsByTagName("button")[0];
+const headerButton = document.querySelectorAll("button")[0];
 headerButton.innerText = siteContent["cta"]["button"];
 // Header Img
-let headerImg = document.getElementById("cta-img");
+const headerImg = document.getElementById("cta-img");
 headerImg.setAttribute('src', siteContent["cta"]["img-src"]);
 
 // ==================Main Content====================
 // Features h4
-let featuresH4 = document.getElementsByTagName("h4")[0];
+const featuresH4 = document.querySelectorAll("h4")[0];
 featuresH4.innerText = siteContent["main-content"]["features-h4"];
 // Features Content
-let featuresContent = document.getElementsByTagName("p")[0];
+const featuresContent = document.querySelectorAll("p")[0];
 featuresContent.innerText = siteContent["main-content"]["features-content"];
 // About h4
-let aboutH4 = document.getElementsByTagName("h4")[1];
+const aboutH4 = document.querySelectorAll("h4")[1];
 aboutH4.innerText = siteContent["main-content"]["about-h4"];
 // About Content
-let aboutContent = document.getElementsByTagName("p")[1];
+const aboutContent = document.querySelectorAll("p")[1];
 aboutContent.innerText = siteContent["main-content"]["about-content"];
 // Middle Image
-let middleImg = document.getElementById("middle-img");
+const middleImg = document.getElementById("middle-img");
 middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 // Services h4
-let servicesH4 = document.getElementsByTagName("h4")[2];
+const servicesH4 = document.querySelectorAll("h4")[2];
 servicesH4.innerText = siteContent["main-content"]["services-h4"];
 // Services Content
-let servicesContent = document.getElementsByTagName("p")[2];
+const servicesContent = document.querySelectorAll("p")[2];
 servicesContent.innerText = siteContent["main-content"]["services-content"];
 // Product h4
-let productH4 = document.getElementsByTagName("h4")[3];
+const productH4 = document.querySelectorAll("h4")[3];
 productH4.innerText = siteContent["main-content"]["product-h4"];
 // Product Content
-let productContent = document.getElementsByTagName("p")[3];
+const productContent = document.querySelectorAll("p")[3];
 productContent.innerText = siteContent["main-content"]["product-content"];
 // Vision h4
-let visionH4 = document.getElementsByTagName("h4")[4];
+const visionH4 = document.querySelectorAll("h4")[4];
 visionH4.innerText = siteContent["main-content"]["vision-h4"];
 // Vision Content
-let visionContent = document.getElementsByTagName("p")[4];
+const visionContent = document.querySelectorAll("p")[4];
 visionContent.innerText = siteContent["main-content"]["vision-content"];
 
 // ==================Contact==================
 // Contact h4
-let contactH4 = document.getElementsByTagName("h4")[5];
+const contactH4 = document.querySelectorAll("h4")[5];
 contactH4.innerText = siteContent["contact"]["contact-h4"];
 // Address
-let address = document.getElementsByTagName("p")[5];
+const address = document.querySelectorAll("p")[5];
 address.innerText = siteContent["contact"]["address"];
 // Phone
-let phone = document.getElementsByTagName("p")[6];
+const phone = document.querySelectorAll("p")[6];
 phone.innerText = siteContent["contact"]["phone"];
 // E-mail
-let email = document.getElementsByTagName("p")[7];
+const email = document.querySelectorAll("p")[7];
 email.innerText = siteContent["contact"]["email"];
 
 // ===================Footer===================
-let copyright = document.getElementsByTagName("p")[8];
+const copyright = document.querySelectorAll("p")[8];
 copyright.innerText = siteContent["footer"]["copyright"];
