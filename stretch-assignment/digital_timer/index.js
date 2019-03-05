9326280833e5bec228cda69113108a84853b0aa2
brellin <cekakid@gmail.com>
2019-03-04 20:28:40 -0600

    added start and stop buttons

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,40 +1,68 @@
-// Set Time of Arrival
-const arrival = new Date();
-let timer;
+// ===============Buttons=============
+// Get Body
+const body = document.querySelector('body');
 
-// Get Time on Page
-function showElapsed() {
-    let rightNow = (new Date()).getTime(),
-        distance = Math.floor((rightNow - arrival) / 10).toString().split(''),
-        sTens = distance[3] == undefined ? 0 : distance[0];
-    if (distance[1] == undefined) {
-        sOnes = 0,
-            mHuns = 0,
-            mTens = distance[0];
-    } else if (distance[2] == undefined) {
-        sOnes = 0,
-            mHuns = distance[0],
-            mTens = distance[1];
-    } else if (distance[3] == undefined) {
-        sOnes = distance[0],
-            mHuns = distance[1],
-            mTens = distance[2];
-    } else {
-        sOnes = distance[1],
-            mHuns = distance[2],
-            mTens = distance[3];
+// Container Div
+const buttons = document.createElement('div');
+body.append(buttons);
+buttons.classList.add('.digits');
+
+// Actual buttons
+const start = document.createElement('button'),
+    stop = document.createElement('button');
+buttons.append(start);
+buttons.append(stop);
+start.innerText = 'Start';
+stop.innerText = 'Stop';
+
+// ===============Timer===============
+// Set Start
+function startTimer() {
+    let arrival = new Date(),
+        timer = setInterval(showElapsed, 1);
+
+    function showElapsed() {
+        let rightNow = (new Date()).getTime(),
+            distance = Math.floor((rightNow - arrival) / 10).toString().split(''),
+            sTens = distance[3] == undefined ? 0 : distance[0];
+        if (distance[1] == undefined) {
+            sOnes = 0,
+                mHuns = 0,
+                mTens = distance[0];
+        } else if (distance[2] == undefined) {
+            sOnes = 0,
+                mHuns = distance[0],
+                mTens = distance[1];
+        } else if (distance[3] == undefined) {
+            sOnes = distance[0],
+                mHuns = distance[1],
+                mTens = distance[2];
+        } else {
+            sOnes = distance[1],
+                mHuns = distance[2],
+                mTens = distance[3];
+        }
+
+        // Get and name digits
+        const secondTens = document.getElementById('secondTens');
+        const secondOnes = document.getElementById('secondOnes');
+        const msHundreds = document.getElementById('msHundreds');
+        const msTens = document.getElementById('msTens');
+
+        // Set individual digit behaviors
+        secondTens.innerText = sTens;
+        secondOnes.innerText = sOnes;
+        msHundreds.innerText = mHuns;
+        msTens.innerText = mTens;
     }
 
-    // Get and name digits
-    const secondTens = document.getElementById('secondTens');
-    const secondOnes = document.getElementById('secondOnes');
-    const msHundreds = document.getElementById('msHundreds');
-    const msTens = document.getElementById('msTens');
-    // Set individual digit behaviors
-    // Second Tens Posion
-    secondTens.innerText = sTens;
-    secondOnes.innerText = sOnes;
-    msHundreds.innerText = mHuns;
-    msTens.innerText = mTens;
+    // Set Stop
+    function stopTimer() {
+        clearInterval(timer);
+    }
+
+    stop.addEventListener('click', stopTimer);
+
 }
-timer = setInterval(showElapsed, 10)
+
+start.addEventListener('click', startTimer);
